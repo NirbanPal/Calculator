c6b77b7dc95bc582debd3322edf8f81d6e174673
Nirban Pal <90189572+NirbanPal@users.noreply.github.com>
2022-05-15 23:12:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let screen =document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(items of buttons){
+    items.addEventListener('click',(num)=>{
+            buttonText = num.target.innerText;
+            if(buttonText == 'x'){
+                buttonText = '*';
+                screenValue += buttonText;
+                screen.value = screenValue;
+            }
+            else if(buttonText == 'C'){
+                screenValue = '';
+                screen.value = screenValue;
+            }
+            else if(buttonText == '='){
+                screen.value = eval(screenValue);
+            }
+            else{
+                screenValue += buttonText;
+                screen.value = screenValue;
+            }
+    })
+}
